e8335ad7b52323042057c791cac50ec8d9f39ea6
Evandro <joseevandro177@gmail.com>
2024-05-23 12:01:57 -0300

    Exclude DataSource configuration

--- src/main/java/br/com/evandro/gestao_vagas/GestaoVagasApplication.java
+++ src/main/java/br/com/evandro/gestao_vagas/GestaoVagasApplication.java
@@ -2,12 +2,13 @@ package br.com.evandro.gestao_vagas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /*
 Annotation responsive for tell to SpringBoot where he will start all the application
 Defines that the class below have the method main
  */
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class GestaoVagasApplication {
 
 	public static void main(String[] args) {
